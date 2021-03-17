180b4e9632a18d8bdf09f7b7f89de86617fa62f3
Charly Casen <BALZAC@MacBook-Air-de-Charly.local>
2021-03-17 18:55:24 +0100

    homepage

--- index.php
+++ index.php
@@ -3,70 +3,63 @@
     <?php include 'config/template/nav.php'; ?>
 </header>
 <body>
-<div class= image_background>
-    <img src="asset/images/aniketh-kanukurthi-Qaor6nxikUM-unsplash.jpg" class="img_fluid" alt="banniere">
-</div> 
-<div class="text">
-    <h1>Qualitea</h1>
-    <h2>Thé & infusions</h2>
-    <h3>Qualitea vous invite à travers une sélection exigeante de thés d'origine exceptionnels 
-et de créations parfumées irrésistibles.</h3>
-</div>
-<div class="small-container">
-    <h3 class="title">Notre sélection du moment</h3>
-    <div class="row">
-        <div class="col_3">
-            <img src="asset/images/the-noir-vietnam-bio.png">
-                <h4>White Bellini Bio</h4>
-                <h6>Recharge 100g</h6>
-                <div class="rating">
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star-o"></i>
-                        <p>18,80€</p>
-                        <button type="button" class="btn btn-primary btn-lg">Ajoutez</button>
-                        
-                </div>
+<div class="home">
+    <div class="titre">
+        <h1 class="big-title">Qualitea</h1>
+        <h2>Thé & infusions</h2>
+    </div>
+        <div class="sous-titre">
+            <p>Qualitea vous invite à travers une sélection exigeante de thés d'origine exceptionnels 
+            et de créations parfumées irrésistibles.</p>
         </div>
-        <div class="col_2">
-            <img src="asset/images/the-vert-sencha-bio-feuille.jpg">
-                <h4>Vert Bio</h4>
-                <h6>Recharge 100g</h6>
-                <div class="rating">
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star-o"></i>
+</div>
+</div>
+    <div class="small-container">
+        <h3 class="title">Notre sélection du moment</h3>
+        <div class="row">
+            <div class="col_3">
+                <img src="asset/images/the-noir-vietnam-bio.png">
+                    <h4>White Bellini Bio</h4>
+                    <h6>Recharge 100g</h6>
+                    <div class="rating">
+                            <p>18,80€</p>
+                            <button type="button" class="btn btn-primary btn-lg">Ajoutez</button>
+                            
+                    </div>
+            </div>
+            <div class="col_2">
+                <img src="asset/images/the-vert-sencha-bio-feuille.jpg">
+                    <h4>Vert Bio</h4>
+                    <h6>Recharge 100g</h6>
+                    <div class="rating">
+                            <p>11,00€</p>
+                            <button type="button" class="btn btn-primary btn-lg">Ajoutez</button>
+                    </div>
+            
+            </div>
+            <div class="col_1">
+                <img src="asset/images/the-noir-keemun.jpg">
+                    <h4>English Breakfast Bio</h4>
+                    <h6>Recharge 100g</h6>
+                    <div class="rating">
                         <p>11,00€</p>
                         <button type="button" class="btn btn-primary btn-lg">Ajoutez</button>
-                </div>
-        
+                    </div>
+            </div>
         </div>
-        <div class="col_1">
-            <img src="asset/images/the-noir-keemun.jpg">
-                <h4>English Breakfast Bio</h4>
-                <h6>Recharge 100g</h6>
-                <div class="rating">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star-o"></i>
-                    <p>11,00€</p>
-                    <button type="button" class="btn btn-primary btn-lg">Ajoutez</button>
-                </div>
+        <div class="row_2">
+            <div class="first_image">
+                <img src="">
+            </div>
         </div>
     </div>
-    <div class="row_2">
-        <div class="first_image">
-            <img src="">
+<div class="offers">
+    <div class="small_container">
+        <div class="">
+
         </div>
+
     </div>
-</div>
-<div class="">
 
 </div>
 </body>
