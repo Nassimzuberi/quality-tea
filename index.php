1fa60ed685a660010b0954fbee0d9443a876a9ad
Charly Casen <BALZAC@MacBook-Air-de-Charly.local>
2021-03-17 17:27:26 +0100

    pagedaccueil

--- index.php
+++ index.php
@@ -2,6 +2,74 @@
 <header>
     <?php include 'config/template/nav.php'; ?>
 </header>
+<body>
+<div class= image_background>
+    <img src="asset/images/aniketh-kanukurthi-Qaor6nxikUM-unsplash.jpg" class="img_fluid" alt="banniere">
+</div> 
+<div class="text">
+    <h1>Qualitea</h1>
+    <h2>Thé & infusions</h2>
+    <h3>Qualitea vous invite à travers une sélection exigeante de thés d'origine exceptionnels 
+et de créations parfumées irrésistibles.</h3>
+</div>
+<div class="small-container">
+    <h3 class="title">Notre sélection du moment</h3>
+    <div class="row">
+        <div class="col_3">
+            <img src="asset/images/the-noir-vietnam-bio.png">
+                <h4>White Bellini Bio</h4>
+                <h6>Recharge 100g</h6>
+                <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                        <p>18,80€</p>
+                        <button type="button" class="btn btn-primary btn-lg">Ajoutez</button>
+                        
+                </div>
+        </div>
+        <div class="col_2">
+            <img src="asset/images/the-vert-sencha-bio-feuille.jpg">
+                <h4>Vert Bio</h4>
+                <h6>Recharge 100g</h6>
+                <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                        <p>11,00€</p>
+                        <button type="button" class="btn btn-primary btn-lg">Ajoutez</button>
+                </div>
+        
+        </div>
+        <div class="col_1">
+            <img src="asset/images/the-noir-keemun.jpg">
+                <h4>English Breakfast Bio</h4>
+                <h6>Recharge 100g</h6>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                    <p>11,00€</p>
+                    <button type="button" class="btn btn-primary btn-lg">Ajoutez</button>
+                </div>
+        </div>
+    </div>
+    <div class="row_2">
+        <div class="first_image">
+            <img src="">
+        </div>
+    </div>
+</div>
+<div class="">
+
+</div>
+</body>
 <h2 class="text-center mt-5 mb-5">Page Accueil</h2>
 <hr>
 <?php include 'config/template/footer.php'; ?>
