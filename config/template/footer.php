589d8eab4fb7fa7ed6d3ab8b6dfc76164d66e86e
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2021-03-22 18:15:38 +0100

    (fix) reorganised css file

--- config/template/footer.php
+++ config/template/footer.php
@@ -1,54 +1,60 @@
-<footer class="container">
-    <div class="row qualities ">
-        <div class="col-3 d-flex">
-            <img src="asset/images/cycle-loading.svg" alt="" class="mx-3">
-            <p>Nos boutiques collectent vos capsules</p>
-        </div>
-        <div class="col-3 d-flex">
-            <img src="asset/images/credit-card.svg" alt="" class="mx-3">
+<footer>
+    <div class="container">
+        <div class="row qualities py-5">
+            <div class="col-3 d-flex">
+                <img src="asset/images/cycle-loading.svg" alt="" class="mx-3">
+                <p>Nos boutiques collectent vos capsules</p>
+            </div>
+            <div class="col-3 d-flex">
+                <img src="asset/images/credit-card.svg" alt="" class="mx-3">
 
-            <p>Paiment
-                sécurisé</p>
+                <p>Paiment
+                    sécurisé</p>
+            </div>
+            <div class="col-3 d-flex">
+                <img src="asset/images/shipped.svg" alt="" class="mx-3">
+                <p>Livraison offerte dès 35€</p>
+            </div>
+            <div class="col-3 d-flex">
+                <p class="footer-brand mx-3">Qualitea</p>
+                <p>Notre programme de fidélité</p>
+            </div>
         </div>
-        <div class="col-3 d-flex">
-            <img src="asset/images/shipped.svg" alt="" class="mx-3">
-            <p>Livraison offerte dès 35€</p>
+
+        <hr style="border-bottom: 2px #7C7C7C solid;" class="my-5">
+
+        <div class="row footer-link">
+            <div class="col-4">
+                <h6>Qualitea</h6>
+                <a href="">La marque</a>
+                <a href="">Programme de fidélité</a>
+                <a href="">Les engagements Qualitea</a>
+                <a href="">Politique de confidentialité</a>
+            </div>
+
+            <div class="col-4">
+                <div>PROSSESIONNEL</div>
+                <a href="">Recrutement</a>
+                <a href="">Contactez-nous</a>
+            </div>
+            <div class="col-4">
+                <div>ASSISTANCE</div>
+                <a href="">Aide et Contact</a>
+                <a href="">Livraison</a>
+                <a href="">Mentions légales</a>
+                <a href="">Avantages et Garanties</a>
+                <a href="">FAQ</a>
+            </div>
         </div>
-        <div class="col-3 d-flex">
-            <p class="footer-brand mx-3">Qualitea</p>
-            <p>Notre programme de fidélité</p>
+
+        <div class="social-link d-flex">
+            <a href=""><img src="../../asset/images/pinterest-social-logo.svg" alt="pinterest"></a>
+            <a href=""><img src="../../asset/images/twitter%20(1).svg" alt=""></a>
+            <a href=""><img src="../../asset/images/instagram.svg" alt=""></a>
+            <a href=""><img src="../../asset/images/facebook.svg" alt=""></a>
         </div>
     </div>
 
-    <hr style="border-bottom: 2px #7C7C7C solid;" class="my-5">
-
-    <div class="row footer-link">
-        <div class="col-4">
-            <h6>Qualitea</h6>
-            <a href="">La marque</a>
-            <a href="">Programme de fidélité</a>
-            <a href="">Les engagements Qualitea</a>
-            <a href="">Politique de confidentialité</a>
-        </div>
-
-        <div class="col-4">
-            <div>PROSSESIONNEL</div>
-            <a href="">Recrutement</a>
-            <a href="">Contactez-nous</a>
-        </div>
-        <div class="col-4">
-            <div>ASSISTANCE</div>
-            <a href="">Aide et Contact</a>
-            <a href="">Livraison</a>
-            <a href="">Mentions légales</a>
-            <a href="">Avantages et Garanties</a>
-            <a href="">FAQ</a>
-        </div>
-    </div>
-
-    <div class="social-link d-flex">
-
-    </div>
 </footer>
 </div>
 
