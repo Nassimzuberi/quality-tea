e5adf2c3b096d33bd2c09cb25e9a9e187c415adf
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2021-04-09 14:56:16 +0200

    (feat) table on admin

--- profil.php
+++ profil.php
@@ -2,17 +2,48 @@
 <header>
     <?php include 'config/template/nav.php'; ?>
 </header>
-<h2 class="text-center mt-5 mb-5">Page profil</h2>
-<p>
 
-</p>
-<?php if(isset($_SESSION["isLogged"])){
-    $user = getUserById($_SESSION['userID']);
-    echo $user['role_id'] == 1 ? "Vous etes un utilisateur" : "Vous êtes un administrateur";
+<div class="background">
+    <h2 class="text-center mt-5 mb-5">Page profil</h2>
+    <?php if(isset($_SESSION["isLogged"])){
+        $user = getUserById($_SESSION['userID']);
+        if($user['role_id'] == 1): ?>
+        Tu es un simple utilisateur
+        <?php elseif ($user['role_id'] == 2) :
+            $articles = getArticles(); ?>
 
-} else {
-    echo "Vous êtes un simple visiteur";
-}
-?>
-<hr>
+            <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nom du produit</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Prix</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+        <?php foreach ($articles as $article):?>
+
+                <tr>
+                    <th scope="row"><?= $article['id'] ?></th>
+                    <td><?= $article['name'] ?></td>
+                    <td><?= $article['stock'] ?></td>
+                    <td><?= $article['prix'] ?></td>
+                    <td>
+                        <a href="./fiche_produit.php?id=<?= $article['id'] ?>" class="btn" target="_blank" rel="noopener">Show</a>
+                        <a href="./produit_update.php?id=<?= $article['id'] ?>" class="btn" target="_blank" rel="noopener">Update</a>
+                    </td>
+                </tr>
+
+        <?php endforeach ?>
+            </tbody>
+            </table>
+        <?php endif;
+    }
+    ?>
+
+    <hr>
+</div>
+
 <?php include 'config/template/footer.php'; ?>
